eda88741f85968210d5e5bd09b105b42a7083915
Nathan Souza <naathanmarinho11@gmail.com>
2025-08-15 18:14:06 -0300

    Add AWS IAM authentication and refactor AppSync resolvers for message handling

--- infra/terraform/modules/appsync/main.tf
+++ infra/terraform/modules/appsync/main.tf
@@ -9,9 +9,13 @@ resource "aws_appsync_graphql_api" "api" {
     default_action = "ALLOW"
   }
 
+  additional_authentication_provider {
+    authentication_type = "AWS_IAM"
+  }
+
   log_config {
     cloudwatch_logs_role_arn = var.cloudwatch_logs_role_arn
-    field_log_level          = var.cloudwatch_logs_role_arn != "" ? "ERROR" : "NONE"
+    field_log_level          = var.cloudwatch_logs_role_arn != "" ? "ALL" : "NONE"
   }
 
   tags = {
@@ -64,15 +68,55 @@ resource "aws_appsync_datasource" "lambda_orchestrator" {
   }
 }
 
-# Resolvers - Basic examples
-resource "aws_appsync_resolver" "send_message" {
+resource "aws_appsync_function" "put_user_message" {
   api_id      = aws_appsync_graphql_api.api.id
-  type        = "Mutation"
-  field       = "sendMessage"
-  data_source = aws_appsync_datasource.lambda_orchestrator.name
+  data_source = aws_appsync_datasource.messages_table.name
+  name        = "PutUserMessage"
 
-  request_template  = file("${path.module}/resolvers/functions/fn_invoke_lambda.vtl")
-  response_template = "$util.toJson($context.result)"
+  request_mapping_template  = file("${path.module}/resolvers/functions/fn_put_user_message.vtl")
+  response_mapping_template = <<EOF
+$util.qr($context.stash.put("userMessage", $context.result))
+$util.toJson($context.result)
+EOF
+}
+
+resource "aws_appsync_function" "invoke_lambda" {
+  api_id      = aws_appsync_graphql_api.api.id
+  data_source = aws_appsync_datasource.lambda_orchestrator.name
+  name        = "InvokeLambda"
+
+  request_mapping_template  = file("${path.module}/resolvers/functions/fn_invoke_lambda.vtl")
+  response_mapping_template = "$util.toJson($context.result)"
+}
+
+resource "aws_appsync_resolver" "send_message" {
+  api_id = aws_appsync_graphql_api.api.id
+  type   = "Mutation"
+  field  = "sendMessage"
+
+  kind = "PIPELINE"
+
+  pipeline_config {
+    functions = [
+      aws_appsync_function.put_user_message.function_id,
+      aws_appsync_function.invoke_lambda.function_id
+    ]
+  }
+
+  request_template = <<EOF
+{
+  "version": "2018-05-29",
+  "operation": "PipelineRequest"
+}
+EOF
+
+  response_template = <<EOF
+#if($ctx.stash.userMessage)
+  $util.toJson($ctx.stash.userMessage)
+#else
+  $util.toJson($ctx.result)
+#end
+EOF
 }
 
 resource "aws_appsync_resolver" "create_user" {
@@ -93,4 +137,64 @@ resource "aws_appsync_resolver" "me" {
 
   request_template  = file("${path.module}/resolvers/functions/fn_get_user_by_email.vtl")
   response_template = file("${path.module}/resolvers/functions/fn_get_user_response.vtl")
+}
+
+resource "aws_appsync_resolver" "list_chats_by_user" {
+  api_id      = aws_appsync_graphql_api.api.id
+  type        = "Query"
+  field       = "listChatsByUser"
+  data_source = aws_appsync_datasource.chats_table.name
+
+  request_template  = file("${path.module}/resolvers/functions/fn_list_chats_by_user.vtl")
+  response_template = "$util.toJson($context.result.items)"
+}
+
+resource "aws_appsync_resolver" "create_chat" {
+  api_id      = aws_appsync_graphql_api.api.id
+  type        = "Mutation"
+  field       = "createChat"
+  data_source = aws_appsync_datasource.chats_table.name
+
+  request_template  = file("${path.module}/resolvers/functions/fn_create_chat.vtl")
+  response_template = "$util.toJson($context.result)"
+}
+
+resource "aws_appsync_resolver" "list_messages" {
+  api_id      = aws_appsync_graphql_api.api.id
+  type        = "Query"
+  field       = "listMessages"
+  data_source = aws_appsync_datasource.messages_table.name
+
+  request_template  = file("${path.module}/resolvers/functions/fn_list_messages.vtl")
+  response_template = "$util.toJson($context.result)"
+}
+
+resource "aws_appsync_resolver" "add_assistant_message" {
+  api_id      = aws_appsync_graphql_api.api.id
+  type        = "Mutation"
+  field       = "addAssistantMessage"
+  data_source = aws_appsync_datasource.messages_table.name
+
+  request_template  = file("${path.module}/resolvers/functions/fn_put_assistant_message.vtl")
+  response_template = "$util.toJson($context.result)"
+}
+
+resource "aws_appsync_resolver" "delete_chat" {
+  api_id      = aws_appsync_graphql_api.api.id
+  type        = "Mutation"
+  field       = "deleteChat"
+  data_source = aws_appsync_datasource.lambda_orchestrator.name
+
+  request_template  = file("${path.module}/resolvers/functions/fn_delete_chat_lambda.vtl")
+  response_template = <<EOF
+#if($context.error)
+  $util.error($context.error.message)
+#else
+  #if($context.result.success)
+    true
+  #else
+    $util.error("Failed to delete chat")
+  #end
+#end
+EOF
 }
