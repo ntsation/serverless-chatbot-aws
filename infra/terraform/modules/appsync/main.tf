d8f03cea9e65ed4cc4533deb80d143cdcb408d65
Nathan Souza <naathanmarinho11@gmail.com>
2025-08-17 01:49:07 -0300

    Update AppSync configuration to use API_KEY authentication and add new outputs for API key and real-time URL

--- infra/terraform/modules/appsync/main.tf
+++ infra/terraform/modules/appsync/main.tf
@@ -1,12 +1,14 @@
 resource "aws_appsync_graphql_api" "api" {
-  authentication_type = "AMAZON_COGNITO_USER_POOLS"
+  authentication_type = "API_KEY"
   name                = "${var.project_name}-api"
   schema              = file(var.schema_path)
 
-  user_pool_config {
-    aws_region     = var.region
-    user_pool_id   = var.cognito_user_pool_id
-    default_action = "ALLOW"
+  additional_authentication_provider {
+    authentication_type = "AMAZON_COGNITO_USER_POOLS"
+    user_pool_config {
+      aws_region   = var.region
+      user_pool_id = var.cognito_user_pool_id
+    }
   }
 
   log_config {
@@ -19,7 +21,18 @@ resource "aws_appsync_graphql_api" "api" {
   }
 }
 
-# Data Sources
+resource "aws_appsync_api_key" "api_key" {
+  api_id      = aws_appsync_graphql_api.api.id
+  description = "API Key for ${var.project_name} AppSync API"
+  expires     = "2025-12-31T23:59:59Z"
+}
+
+resource "aws_appsync_datasource" "none" {
+  api_id = aws_appsync_graphql_api.api.id
+  name   = "None"
+  type   = "NONE"
+}
+
 resource "aws_appsync_datasource" "users_table" {
   api_id           = aws_appsync_graphql_api.api.id
   name             = "UsersTable"
@@ -64,15 +77,39 @@ resource "aws_appsync_datasource" "lambda_orchestrator" {
   }
 }
 
-# Resolvers - Basic examples
+resource "aws_appsync_function" "put_user_message" {
+  api_id      = aws_appsync_graphql_api.api.id
+  data_source = aws_appsync_datasource.messages_table.name
+  name        = "PutUserMessage"
+  runtime {
+    name            = "APPSYNC_JS"
+    runtime_version = "1.0.0"
+  }
+  code = file("${path.module}/functions/fn_put_user_message.js")
+}
+
+resource "aws_appsync_function" "invoke_orchestrator" {
+  api_id      = aws_appsync_graphql_api.api.id
+  data_source = aws_appsync_datasource.lambda_orchestrator.name
+  name        = "InvokeOrchestrator"
+  runtime {
+    name            = "APPSYNC_JS"
+    runtime_version = "1.0.0"
+  }
+  code = file("${path.module}/functions/fn_invoke_lambda.js")
+}
+
 resource "aws_appsync_resolver" "send_message" {
   api_id      = aws_appsync_graphql_api.api.id
   type        = "Mutation"
   field       = "sendMessage"
-  data_source = aws_appsync_datasource.lambda_orchestrator.name
+  data_source = aws_appsync_datasource.messages_table.name
 
-  request_template  = file("${path.module}/resolvers/functions/fn_invoke_lambda.vtl")
-  response_template = "$util.toJson($context.result)"
+  runtime {
+    name            = "APPSYNC_JS"
+    runtime_version = "1.0.0"
+  }
+  code = file("${path.module}/functions/fn_put_user_message.js")
 }
 
 resource "aws_appsync_resolver" "create_user" {
@@ -81,8 +118,11 @@ resource "aws_appsync_resolver" "create_user" {
   field       = "createUser"
   data_source = aws_appsync_datasource.users_table.name
 
-  request_template  = file("${path.module}/resolvers/functions/fn_create_user.vtl")
-  response_template = "$util.toJson($context.result)"
+  runtime {
+    name            = "APPSYNC_JS"
+    runtime_version = "1.0.0"
+  }
+  code = file("${path.module}/functions/fn_create_user.js")
 }
 
 resource "aws_appsync_resolver" "me" {
@@ -91,6 +131,88 @@ resource "aws_appsync_resolver" "me" {
   field       = "me"
   data_source = aws_appsync_datasource.users_table.name
 
-  request_template  = file("${path.module}/resolvers/functions/fn_get_user_by_email.vtl")
-  response_template = file("${path.module}/resolvers/functions/fn_get_user_response.vtl")
+  runtime {
+    name            = "APPSYNC_JS"
+    runtime_version = "1.0.0"
+  }
+  code = file("${path.module}/functions/fn_get_user_by_email.js")
+}
+
+resource "aws_appsync_resolver" "create_chat" {
+  api_id      = aws_appsync_graphql_api.api.id
+  type        = "Mutation"
+  field       = "createChat"
+  data_source = aws_appsync_datasource.chats_table.name
+
+  runtime {
+    name            = "APPSYNC_JS"
+    runtime_version = "1.0.0"
+  }
+  code = file("${path.module}/functions/fn_create_chat.js")
+}
+
+resource "aws_appsync_resolver" "list_chats_by_user" {
+  api_id      = aws_appsync_graphql_api.api.id
+  type        = "Query"
+  field       = "listChatsByUser"
+  data_source = aws_appsync_datasource.chats_table.name
+
+  runtime {
+    name            = "APPSYNC_JS"
+    runtime_version = "1.0.0"
+  }
+  code = file("${path.module}/functions/fn_list_chats.js")
+}
+
+resource "aws_appsync_resolver" "list_messages" {
+  api_id      = aws_appsync_graphql_api.api.id
+  type        = "Query"
+  field       = "listMessages"
+  data_source = aws_appsync_datasource.messages_table.name
+
+  runtime {
+    name            = "APPSYNC_JS"
+    runtime_version = "1.0.0"
+  }
+  code = file("${path.module}/functions/fn_list_messages.js")
+}
+
+resource "aws_appsync_resolver" "add_assistant_message" {
+  api_id      = aws_appsync_graphql_api.api.id
+  type        = "Mutation"
+  field       = "addAssistantMessage"
+  data_source = aws_appsync_datasource.messages_table.name
+
+  runtime {
+    name            = "APPSYNC_JS"
+    runtime_version = "1.0.0"
+  }
+  code = file("${path.module}/functions/fn_add_assistant_message.js")
+}
+
+resource "aws_appsync_resolver" "delete_chat" {
+  api_id      = aws_appsync_graphql_api.api.id
+  type        = "Mutation"
+  field       = "deleteChat"
+  data_source = aws_appsync_datasource.lambda_orchestrator.name
+
+  runtime {
+    name            = "APPSYNC_JS"
+    runtime_version = "1.0.0"
+  }
+  code = file("${path.module}/functions/fn_delete_chat.js")
+}
+
+# Subscription resolvers for real-time functionality
+resource "aws_appsync_resolver" "on_message_sent" {
+  api_id      = aws_appsync_graphql_api.api.id
+  type        = "Subscription"
+  field       = "onMessageSent"
+  data_source = aws_appsync_datasource.none.name
+
+  runtime {
+    name            = "APPSYNC_JS"
+    runtime_version = "1.0.0"
+  }
+  code = file("${path.module}/functions/fn_subscription.js")
 }
