eccb73dc6c9d46bdb74dc5ff652867dc25262239
Nathan Souza <naathanmarinho11@gmail.com>
2025-08-17 01:42:58 -0300

    Remove frontend components and services for AWS Serverless Chatbot

--- frontend/components/__init__.py
+++
@@ -1,9 +0,0 @@
-from .login import render_login_page
-from .sidebar import render_sidebar
-from .chat import render_chat_page
-
-__all__ = [
-    'render_login_page',
-    'render_sidebar', 
-    'render_chat_page'
-]
