5a425bba8a4b88cf1d6b2925afa0d54054af5623
Nathan Souza <naathanmarinho11@gmail.com>
2025-08-18 20:41:47 -0300

    Refactor request function to include userMessage in payload arguments

--- infra/terraform/modules/appsync/functions/fn_invoke_lambda.js
+++ infra/terraform/modules/appsync/functions/fn_invoke_lambda.js
@@ -1,15 +1,22 @@
 import { util } from '@aws-appsync/utils';
 
 export function request(ctx) {
+  // Se for parte de um pipeline, use o resultado da função anterior
+  const userMessage = ctx.prev.result;
+  
   return {
     operation: 'Invoke',
     payload: {
-      arguments: ctx.arguments,
+      arguments: {
+        chatId: ctx.arguments.chatId || userMessage.chatId,
+        content: ctx.arguments.content || userMessage.content
+      },
       identity: {
         sub: ctx.identity.sub,
         username: ctx.identity.username
       },
-      source: ctx.source
+      source: ctx.source,
+      userMessage: userMessage
     }
   };
 }
